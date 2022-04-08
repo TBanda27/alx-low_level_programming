cf433086e828a8140fa915e6391aeb0c994b5a7a
TBanda27 <tzbanda@outlook.com>
2022-04-08 12:24:37 -0700

    alphabet in small cases

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main 
- *
- * Return: Always (Success)
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char c;
-
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-
 	putchar('\n');
 	return (0);
-
 }
 
