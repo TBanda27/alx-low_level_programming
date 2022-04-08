4e5bdd3e2f2940cad1c1e5ffd97c80a391d961e5
TBanda27 <tzbanda@outlook.com>
2022-04-08 12:16:46 -0700

    updated alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
- * Return: Always 0 (Success)
+ * main 
+ *
+ * Return: Always (Success)
  */
 int main(void)
 {
-    int ch;
-    for (ch = 'a'; ch <= 'z'; ch++)
+	char c;
 
-        putchar(ch);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
-    putchar('\n');
-    getch();
+	putchar('\n');
+	return (0);
+
 }
+
