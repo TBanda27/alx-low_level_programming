2d4693475f0d52ce0f4f981b3a6f91e07dd347fe
TBanda27 <tzbanda@outlook.com>
2022-04-08 12:38:25 -0700

    Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,18 +5,15 @@
  *
  * Return: Always (Success)
  */
+
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
+		putchar(c)
 	for (c = 'A'; c <= 'Z'; c++)
-	{
 		putchar(c);
-	}
 	putchar('\n');
 	return (0);
 }
