ed26d18e00eabe8c94b884862866606fd824d999
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 18:01:56 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * _islower - returns 1 is lowercase, 0 otherwise
+ * _isalpha - returns 1 is lowercase, 0 otherwise
  * @c: Input parameter in ASCII
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
