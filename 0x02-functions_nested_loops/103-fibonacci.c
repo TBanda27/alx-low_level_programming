20b71ea5b031387903c42b9f6dd2f63fff742f43
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 18:53:58 +0200

    Sum of even fibonaci values less than 400000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - funcion firts 50 fibonacci
+* void - no value into
+* Return: always zero
+*/
+
+int main(void)
+{
+	int i;
+	long int j, a, b, k;
+
+	j = 0;
+	a = 0;
+	b = 1;
+	k = 0;
+
+	for (i = 0; j <= 4000000; i++)
+	{
+		j = a + b;
+		a = b;
+		b = j;
+
+		if (j % 2 == 0 && j != 4000000)
+		{
+			k = k + j;
+		}
+	}
+	printf("%ld", k);
+	printf("\n");
+	return (0);
+}
