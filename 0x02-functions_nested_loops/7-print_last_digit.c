ebbd777ea7c20e08e6f0550554c8696bc2ff46f8
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 18:13:02 +0200

    Print the last digit of an integer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - returns 1 is lowercase, 0 otherwise
+ * @x: Input parameter in ASCII
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_last_digit(int x)
+{
+	x = x % 10;
+
+	if (x < 0)
+		x = x * (-1);
+
+	_putchar('0' + x);
+	return (x);
+}
