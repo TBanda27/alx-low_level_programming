5056d1b02735988a8bbee46e6be9a0052460b78c
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 17:28:02 +0200

    Check whether input is lowercase or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*3-islower - check if input is lowercase
+* Return: Always 0 (Success)
+*/
+int _islower(int c){
+  if (c.islower()){
+    return (1);
+  }
+  else{
+    return (0);
+    )
+}
