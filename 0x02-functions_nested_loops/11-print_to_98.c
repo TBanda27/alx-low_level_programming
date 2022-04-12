4abfd1aed229e6c4b6a0a3bc2dac9ce3d3a0abc5
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 18:31:43 +0200

    Print all numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - returns 1 is lowercase, 0 otherwise
+ * @n : input paramether
+ * Return: alwais 0;
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_to_98(int n)
+{
+	int a;
+
+	if (n == 98)
+	{
+	printf("%d", n);
+	}
+	else if (n < 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			if (a == 98)
+			{
+			printf("%d", a);
+			}
+			else
+			{
+			printf("%d, ", a);
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (a = n; a >= 98; a--)
+		{
+			if (a == 98)
+			{
+			printf("%d", a);
+			}
+			else
+			{
+			printf("%d, ", a);
+			}
+		}
+	}
+	printf("\n");
+}
