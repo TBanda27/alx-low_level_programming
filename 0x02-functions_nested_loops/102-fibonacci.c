59cd15ec0c543ddb1e1c153eba64958d9004de37
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 18:46:39 +0200

    50 fibonaci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - funcion firts 50 fibonacci
+* void - no value into
+* Return: always zero
+*/
+int main(void)
+{
+	int i;
+	long int j, a, b;
+
+	j = 0;
+	a = 0;
+	b = 1;
+
+	for (i = 0; i <= 49; i++)
+	{
+		j = a + b;
+		a = b;
+		b = j;
+
+		printf("%ld", j);
+
+		if (i != 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
