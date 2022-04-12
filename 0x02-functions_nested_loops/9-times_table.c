18b0942029e1862dc561afcbbd761f69da80ad91
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 18:24:12 +0200

    9's timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,33 +16,33 @@ void times_table(void)
 			c = a * b;
 			if (c < 10 && b == 9)
 			{
-        _putchar(32);
-        _putchar('0' + c);
+				_putchar(32);
+				_putchar('0' + c);
 			}
 			else if (c >= 10 && b == 9)
 			{
-        _putchar('0' + c / 10);
-        _putchar('0' + c % 10);
+				_putchar('0' + c / 10);
+				_putchar('0' + c % 10);
 			}
 			else if (b == 0)
 			{
-        _putchar('0' + c);
-        _putchar(44);
-        _putchar(32);
+				_putchar('0' + c);
+				_putchar(44);
+				_putchar(32);
 			}
 			else if (c < 10)
 			{
-        _putchar(32);
-        _putchar('0' + c);
-        _putchar(44);
-        _putchar(32);
+				_putchar(32);
+				_putchar('0' + c);
+				_putchar(44);
+				_putchar(32);
 			}
 			else
 			{
-        _putchar('0' + c / 10);
-        _putchar('0' + c % 10);
-        _putchar(44);
-        _putchar(32);
+				_putchar('0' + c / 10);
+				_putchar('0' + c % 10);
+				_putchar(44);
+				_putchar(32);
 			}
 		}
 		_putchar(10);
