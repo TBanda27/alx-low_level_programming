18a68988cbba59cc36e7edba8fced2f9e0de889b
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 18:38:02 +0200

    Print all natural numbers in range

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - function de sum all the number below 1024 multiples 3 or 5
+* void - no value into
+* Return: always cero.
+*/
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j = j + i;
+		}
+	}
+	printf("%d\n", j);
+	return (0);
+}
