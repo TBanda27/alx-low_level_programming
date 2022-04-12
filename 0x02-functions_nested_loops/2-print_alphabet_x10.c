ffb629f50cadafef3d49b78357f3ec87b47d0fc5
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 17:12:57 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,14 @@
 void print_alphabet_x10(void)
 {
 	int a;
-  int i;
-  
-  for (i = 0; i < 10; i++)
-  {
-    for (a = 'a'; a <= 'z'; a++)
-    {
-      _putchar(a);
-    }
-	  _putchar('\n');
-  }
+	int i;
 	
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
 }
