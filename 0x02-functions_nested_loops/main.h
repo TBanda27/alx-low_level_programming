e66fb565abaf2730173889085ccfa48841f4e49e
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 15:55:54 +0200

    Main header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,34 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/* _putchar - writes the character c to stdout*/
+int _putchar(char c);
+/* print_alphabet(); print alphabet to lowercase*/
+void print_alphabet(void);
+/* print_alphabet(); print alphabet to lowercase */
+void print_alphabet_x10(void);
+/*int_inslower:wq!*/
+int _islower(int c);
+/*write a funtion that chechs for alphabetic*/
+int _isalpha(int c);
+/*write the funtion that prints the sign of a number*/
+int print_sign(int n);
+/*write a function that computes the absolute value of an integer*/
+int _abs(int);
+/*write a function thath prints the last digit of a number*/
+int print_last_digit(int);
+/*Write a function that prints every minute of the day of Jack Bauer, starting*/
+void jack_bauer(void);
+/*write a function that prints the 9 times table, starting with 0*/
+void times_table(void);
+/*Write a function that adds two integers and returns the result.*/
+int add(int, int);
+/*Write a function that prints all natural numbers from n to 98, followed by a new line.*/
+void print_to_98(int n);
+/**/
+void print_times_table(int n);
+/**/
+
+
+
+#endif
