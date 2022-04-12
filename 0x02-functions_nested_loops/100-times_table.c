21c11eef312d76bfc5077a576d7b7340cafbd088
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 18:35:05 +0200

    Multiplication table of any given number

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * print_times_table - returns 1 is lowercase, 0 otherwise
+ * @n: no input paramether
+ * Return: alwais 0;
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_times_table(int n)
+{
+	int a, i, j, k;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (j == 0)
+					_putchar('0' + k);
+				else if (k < 10)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(32);
+					_putchar('0' + k);
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar('0' + k / 10);
+					_putchar('0' + k % 10);
+				}
+				else if (k >= 100 && k < 1000)
+				{
+					a = k / 10;
+					_putchar(32);
+					_putchar('0' + a / 10);
+					_putchar('0' + a % 10);
+					_putchar('0' + k % 10);
+				}
+				if (j != n)
+					_putchar(44);
+			}
+			_putchar(10);
+		}
+	}
+}
