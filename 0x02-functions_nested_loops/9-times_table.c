7857fe789adcac37ff9732cc8e7859cc5efee360
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 18:19:52 +0200

    Multiplication table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * times_table - returns 1 is lowercase, 0 otherwise
+ * void: no input paramether
+ * Return: alwais 0;
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c < 10 && b == 9)
+			{
+        _putchar(32);
+        _putchar('0' + c);
+			}
+			else if (c >= 10 && b == 9)
+			{
+        _putchar('0' + c / 10);
+        _putchar('0' + c % 10);
+			}
+			else if (b == 0)
+			{
+        _putchar('0' + c);
+        _putchar(44);
+        _putchar(32);
+			}
+			else if (c < 10)
+			{
+        _putchar(32);
+        _putchar('0' + c);
+        _putchar(44);
+        _putchar(32);
+			}
+			else
+			{
+        _putchar('0' + c / 10);
+        _putchar('0' + c % 10);
+        _putchar(44);
+        _putchar(32);
+			}
+		}
+		_putchar(10);
+	}
+}
