7b83e9b606603f25404f84a24057ace294e36e31
TBanda27 <68060987+TBanda27@users.noreply.github.com>
2022-04-12 17:30:10 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,14 @@
 *3-islower - check if input is lowercase
 * Return: Always 0 (Success)
 */
-int _islower(int c){
-  if (c.islower()){
+int _islower(int c)
+{
+  if (islower(c))
+  {
     return (1);
   }
-  else{
+  else
+  {
     return (0);
-    )
+  }
 }
